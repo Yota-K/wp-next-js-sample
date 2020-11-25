25bc0889adb071506d7c906c86b806c0d45d1b08
紅梅陽太 <powdersugar828828@gmail.com>
2020-11-25 23:19:37 +0900

    ブロックの大枠作成。

--- app/wp-content/plugins/custom-block-rating/src/block/block.js
+++ app/wp-content/plugins/custom-block-rating/src/block/block.js
@@ -1,99 +1,87 @@
-/**
- * BLOCK: custom-block-rating
- *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
- */
+import { registerBlockType } from '@wordpress/blocks';
+import { RichText } from '@wordpress/block-editor';
+import { SelectControl } from '@wordpress/components';
 
 //  Import CSS.
 import './editor.scss';
 import './style.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
 registerBlockType( 'cgb/block-custom-block-rating', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'custom-block-rating - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [
-		__( 'custom-block-rating — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
-	],
+  title: 'レーティング用のブロック',
+  description: 'レーティングを表示するためのブロックです。',
+  // MEMO: アイコン一覧 https://developer.wordpress.org/resource/dashicons/
+  icon: 'star-empty',
+  category: 'layout',
+  // MEMO: 検索する際のキーワードを指定
+  keywords: [
+    'レーティング',
+    '装飾',
+  ],
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Component.
-	 */
-	edit: ( props ) => {
-		// Creates a <p class='wp-block-cgb-block-custom-block-rating'></p>.
-		return (
-			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>custom-block-rating</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
-			</div>
-		);
-	},
+  // MEMO: 入力された値を保存するための属性を追加
+  attributes: {
+    ratingTitle: {
+      type: 'string',
+      default: '',
+    },
+    ratingSelect: {
+      type: 'string',
+      default: '',
+    },
+    ratingText: {
+      type: 'string',
+      default: '',
+    },
+  },
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Frontend HTML.
-	 */
-	save: ( props ) => {
-		return (
-			<div className={ props.className }>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>custom-block-rating</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
-			</div>
-		);
-	},
-} );
+  edit: ({ attributes: { ratingTitle, ratingSelect, ratingText }, setAttributes }) => {
+    const options = [
+      { value: '-', label: '評価を選択してください' },
+      { value: 1, label: '★' },
+      { value: 2, label: '★★' },
+      { value: 3, label: '★★★' },
+      { value: 4, label: '★★★★' },
+      { value: 5, label: '★★★★★' },
+    ];
+
+    return (
+      <div className="wp-block-cgb-block-my-block rating-content">
+        <RichText
+          value={ratingTitle}
+          onChange={newText => setAttributes({ ratingTitle: newText })}
+          tagName="h2"
+          placeholder="タイトルを入力してください"
+          keepPlaceholderOnFocus={true}
+        />
+        <SelectControl
+          options={options}
+          value={ratingSelect}
+          onChange={newId => setAttributes({ ratingSelect: newId })}
+        />
+        <RichText
+          value={ratingText}
+          onChange={newText => setAttributes({ ratingText: newText })}
+          tagName="p"
+          placeholder="本文を入力してください"
+          keepPlaceholderOnFocus={true}
+        />
+      </div>
+    );
+  },
+
+  save: ({ attributes: { ratingTitle, ratingSelect, ratingText } }) => {
+    return (
+      <div className="rating-content">
+        <RichText.Content value={ratingTitle} tagName="h2" />
+        <div className={`rating-${ ratingSelect }`}>
+          <span className="star">★</span>
+          <span className="star">★</span>
+          <span className="star">★</span>
+          <span className="star">★</span>
+          <span className="star">★</span>
+        </div>
+        <RichText.Content value={ratingText} tagName="p" />
+      </div>
+    );
+  },
+});
